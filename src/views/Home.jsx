f4649dc64b01b941993957050a06610c12858c57
pengpon <bd102pon@gmail.com>
2023-11-14 23:41:45 +0800

    feat(component): add issues layout

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/layout/Header'
+import Issues  from '../components/layout/Issues'
 
 export default function Home () {
   return (
     <>
     <Header/>
     <div className='container'>
+      <Issues title="政策議題" subtitle="Policy Issues"/>
     </div>
     </>
   )
