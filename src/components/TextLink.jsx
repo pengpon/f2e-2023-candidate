07343b816b517333f6818238b38c655d39bb8c4f
pengpon <bd102pon@gmail.com>
2023-11-14 16:41:21 +0800

    feat(component): add text link

---
+++ src/components/TextLink.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+function TextLink({theme, href, children}) {
+  return (
+    <>
+      <a href={href} className={`text-link is-${theme}`} >{children}</a>
+    </>
+  )
+}
+
+TextLink.propTypes = {
+  children: PropTypes.node,
+  href: PropTypes.string,
+  theme: PropTypes.string
+};
+
+export default TextLink;
