c3d78160363f2fda3d2d9dbdb0d21cc61f4b0427
pengpon <bd102pon@gmail.com>
2023-11-14 23:33:51 +0800

    feat(component): add tabs

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
 import { ReactSVG } from "react-svg";
 
-function Icon({content}) {
+function Icon({content, size}) {
+  if (!size) {
+    size = 32;
+  }
   return (
     <>
-      <ReactSVG className="icon" src={`./icons/${content}.svg`} />
+      <ReactSVG
+        className="icon"
+        style={{width: `${size}px`}}
+        src={`./icons/${content}.svg`}
+      />
     </>
   )
 }
 
 Icon.propTypes = {
   content: PropTypes.string,
+  size: PropTypes.number,
 };
 
 export default Icon;
